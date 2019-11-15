a009ac835dec56e8d3ee8be1f077a85c655afc33
Huang Kaiwen <huangkaiw3n@gmail.com>
2019-11-15 13:22:12 +0800

    Show user location

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -30,6 +30,7 @@ export default function HomeScreen() {
       <MapView
         style={ styles.mapStyle }
         initialRegion={ SINGAPORE_REGION }
+        showsUserLocation={ true }
       />
     </View>
   )
